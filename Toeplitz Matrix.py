2187aeeaea6d0794164f93d48b849ffa1b7d8f21
fetehadin <fetehadinnegash@gmail.com>
2025-07-08 16:53:33 +0300

    Create Toeplitz Matrix.py

---
+++ Toeplitz Matrix.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        rows, columns = len(matrix), len(matrix[0])
+
+        for r in range(1, rows):
+            for c in range(1, columns):
+                if matrix[r][c] != matrix[r-1][c-1]:
+                    return False
+        return True
