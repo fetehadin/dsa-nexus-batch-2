80ad346daef1030b5277853b7a0a090fc1df564f
fetehadin <fetehadinnegash@gmail.com>
2025-07-26 18:25:50 +0300

    Create Find Target Indices After Sorting Array.py

---
+++ Find Target Indices After Sorting Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        return [i for i , num in enumerate(nums) if target == num]
+                
+        
+        
