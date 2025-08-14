c5e7dfaf53eaff17465c0ae37144aab7e79b1908
fetehadin <fetehadinnegash@gmail.com>
2025-08-14 21:48:32 +0300

    Update Maximum Average Subarray I.py

--- Maximum Average Subarray I.py
+++ Maximum Average Subarray I.py
@@ -1,6 +1,6 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        win_sum = sum(nums[:k])
+        win_sum =sum(nums[:k])
         max_sum = win_sum/k
         for num in range(k,len(nums)):
             win_sum += nums[num]
